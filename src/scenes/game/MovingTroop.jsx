b91f62fbec5265eba3bcb81935d5e6f46cab162a
Michele Lucini <mic.lucini@gmail.com>
2024-06-22 17:53:23 +0200

    Moving troops

--- src/scenes/game/MovingTroop.jsx
+++ src/scenes/game/MovingTroop.jsx
@@ -13,31 +13,53 @@ const MovingTroop = ({
   const endingPositionTop = fromTileToPx(startingTile.y - data.destinationy);
   const endingPositionLeft = fromTileToPx(data.destinationX - startingTile.x);
 
+  const deltaX = endingPositionLeft - startingPositionLeft;
+  const deltaY = endingPositionTop - startingPositionTop;
+
   const elapsedTime = data.travelTotal - data.travelRemaining;
 
-  const currentTop = startingPositionTop + (elapsedTime / data.travelTotal) * (endingPositionTop - startingPositionTop);
-  const currentLeft = startingPositionLeft + (elapsedTime / data.travelTotal) * (endingPositionLeft - startingPositionLeft);
+  const currentTop = startingPositionTop + (elapsedTime / data.travelTotal) * deltaY;
+  const currentLeft = startingPositionLeft + (elapsedTime / data.travelTotal) * deltaX;
+
+  // Creazione della linea di percorso
+  const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+  // Calcolo dell'angolo della linea
+  const angle = Math.atan2(deltaY, deltaX) * 180 / Math.PI;
 
   return (
-    <div
-      style={{
-        position: "absolute",
-        top: currentTop,
-        left: currentLeft,
-        height: 30,
-        width: 30,
-        marginTop: -15,
-        marginLeft: -15,
-        backgroundColor: data.isMine ? "aquamarine" : "darksalmon",
-        borderRadius: "50%",
-        outline: "4px solid rgba(0,0,0,0.1)",
-        display: "grid",
-        placeContent: "center",
-        textAlign: "center",
-      }}
-    >
-      <small>{data.amount}</small>
-    </div>
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0,0,0,0.2)",
+          top: startingPositionTop,
+          left: startingPositionLeft,
+          height: 2,
+          width: distance,
+          transformOrigin: "0 0",
+          transform: `rotate(${angle}deg)`,
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: currentTop,
+          left: currentLeft,
+          height: 30,
+          width: 30,
+          marginTop: -15,
+          marginLeft: -15,
+          backgroundColor: data.isMine ? "aquamarine" : "darksalmon",
+          borderRadius: "50%",
+          outline: "4px solid rgba(0,0,0,0.1)",
+          display: "grid",
+          placeContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <small>{data.amount}</small>
+      </div>
+    </>
   );
 };
 
